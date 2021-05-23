0f41f786efdaae49d6ef5e96058e5a39e8e0e933
Flamy <27062841+f1amy@users.noreply.github.com>
2021-05-23 18:55:58 +0500

    add users controller - update and get info

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -3,30 +3,49 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class UserController extends Controller
 {
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\UserResource
      */
-    public function show($id)
+    public function show(Request $request)
     {
-        //
+        return new UserResource($request->user());
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\UpdateUserRequest $request
+     * @return \App\Http\Resources\UserResource
      */
-    public function update(Request $request, $id)
+    public function update(UpdateUserRequest $request)
     {
-        //
+        /**
+         * @var \App\Models\User $user
+         */
+        $user = $request->user();
+
+        $attributes = Arr::get($request->validated(), 'user');
+
+        if ($request->hasFile('user.image')) {
+            /**
+             * @var \Illuminate\Http\UploadedFile
+             */
+            $image = $request->file('user.image');
+
+            $attributes['image'] = $image->path();
+        }
+
+        $user->update($attributes);
+
+        return new UserResource($user);
     }
 }
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $user = $this->user();
+
+        return [
+            'user.username' => [
+                'required_without_all:user.email,user.bio,user.image',
+                'alpha_num',
+                'max:255',
+                Rule::unique('users', 'username')->ignore($user->getKey()),
+            ],
+            'user.email' => [
+                'required_without_all:user.username,user.bio,user.image',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($user->getKey()),
+            ],
+            'user.bio' => 'required_without_all:user.username,user.email,user.image|string',
+            'user.image' => 'required_without_all:user.username,user.email,user.bio|file|image',
+        ];
+    }
+}
