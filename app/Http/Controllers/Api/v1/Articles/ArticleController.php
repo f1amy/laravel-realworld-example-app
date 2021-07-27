62f509d9b66888b668a907478ec40d07c51dd97c
Flamy <27062841+f1amy@users.noreply.github.com>
2021-07-27 13:36:11 +0500

    finalize article
    - implement create, update, feed and global listing for articles

--- app/Http/Controllers/Api/v1/Articles/ArticleController.php
+++ app/Http/Controllers/Api/v1/Articles/ArticleController.php
@@ -3,44 +3,123 @@
 namespace App\Http\Controllers\Api\v1\Articles;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ArticleListRequest;
+use App\Http\Requests\FeedRequest;
 use App\Http\Requests\NewArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
 use App\Http\Resources\ArticleResource;
 use App\Http\Resources\ArticlesCollection;
 use App\Models\Article;
-use Illuminate\Http\Request;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 
 class ArticleController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @var int Default limit for feed listing.
      */
-    public function list()
+    protected const FILTER_LIMIT = 20;
+
+    /***
+     * @var int Default offset for feed listing.
+     */
+    protected const FILTER_OFFSET = 0;
+
+    /**
+     * Display global listing of the articles.
+     *
+     * @param \App\Http\Requests\ArticleListRequest $request
+     * @return \App\Http\Resources\ArticlesCollection<Article>
+     */
+    public function list(ArticleListRequest $request)
     {
-        //
+        $filter = collect($request->validated());
+
+        $limit = (int) $filter->get('limit', static::FILTER_LIMIT);
+        $offset = (int) $filter->get('offset', static::FILTER_OFFSET);
+
+        $list = Article::orderBy('created_at', 'desc')
+            ->limit($limit)
+            ->offset($offset);
+
+        if ($tag = $filter->get('tag')) {
+            $list->whereHas('tags', function (Builder $query) use ($tag) {
+                $query->where('name', $tag);
+            });
+        }
+
+        if ($authorName = $filter->get('author')) {
+            $list->whereHas('author', function (Builder $query) use ($authorName) {
+                $query->where('name', $authorName);
+            });
+        }
+
+        if ($userName = $filter->get('favorited')) {
+            $list->whereHas('favoredUsers', function (Builder $query) use ($userName) {
+                $query->where('name', $userName);
+            });
+        }
+
+        return new ArticlesCollection($list->get());
     }
 
     /**
-     * Display a listing of the resource.
+     * Display article feed for the user.
      *
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\FeedRequest $request
+     * @return \App\Http\Resources\ArticlesCollection<Article>
      */
-    public function feed()
+    public function feed(FeedRequest $request)
     {
-        //
+        $filter = collect($request->validated());
+
+        $limit = (int) $filter->get('limit', static::FILTER_LIMIT);
+        $offset = (int) $filter->get('offset', static::FILTER_OFFSET);
+
+        /**
+         * @var \App\Models\User $user
+         */
+        $user = $request->user();
+
+        $feed = Article::whereHas('author',
+            function (Builder $query) use ($user) {
+                $query->whereIn('id', $user->authors->pluck('id'));
+            })
+            ->orderBy('created_at', 'desc')
+            ->limit($limit)
+            ->offset($offset)
+            ->get();
+
+        return new ArticlesCollection($feed);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\NewArticleRequest $request
+     * @return \App\Http\Resources\ArticleResource
      */
-    public function create(Request $request)
+    public function create(NewArticleRequest $request)
     {
-        //
+        $attributes = Arr::get($request->validated(), 'article');
+        $tags = Arr::pull($attributes, 'tagList');
+
+        $article = Article::create($attributes);
+
+        if (is_array($tags)) {
+            foreach ($tags as $tagName) {
+                $tag = Tag::firstOrCreate([
+                    'name' => $tagName,
+                ]);
+
+                $article->tags()->attach($tag);
+            }
+
+            $article->refresh();
+        }
+
+        return new ArticleResource($article);
     }
 
     /**
@@ -60,13 +139,23 @@ class ArticleController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\UpdateArticleRequest $request
+     * @param string $slug
+     * @return \App\Http\Resources\ArticleResource
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
-    public function update(Request $request, $id)
+    public function update(UpdateArticleRequest $request, string $slug)
     {
-        //
+        $article = Article::whereSlug($slug)
+            ->firstOrFail();
+
+        $this->authorize('update', $article);
+
+        $attributes = Arr::get($request->validated(), 'article');
+
+        $article->update($attributes);
+
+        return new ArticleResource($article);
     }
 
     /**
--- app/Http/Requests/UpdateArticleRequest.php
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -4,10 +4,25 @@ namespace App\Http\Requests;
 
 use App\Models\Article;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
 class UpdateArticleRequest extends FormRequest
 {
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'article.slug' => Str::slug(
+                (string) $this->input('article.title')
+            ),
+        ]);
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -15,15 +30,17 @@ class UpdateArticleRequest extends FormRequest
      */
     public function rules()
     {
-        $unique = Rule::unique('articles', 'title');
+        $article = Article::whereSlug($this->route('slug'))
+            ->first();
 
-        $article = Article::whereSlug($this->route('slug'))->first();
+        $unique = Rule::unique('articles', 'slug');
         if ($article !== null) {
             $unique->ignoreModel($article);
         }
 
         return [
-            'article.title' => ['required', 'string', $unique],
+            'article.title' => 'required|string',
+            'article.slug' => ['required', 'string', $unique],
             'article.description' => 'required|string',
             'article.body' => 'required|string',
         ];
