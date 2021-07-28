c31284ea18530530c3750ac0118f9683b4bdd5cf
Flamy <27062841+f1amy@users.noreply.github.com>
2021-07-28 16:57:34 +0500

    update user factory
    - update composer dependencies

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->unique()->userName(),
             'email' => $this->faker->unique()->safeEmail(),
+            'bio' => $this->faker->paragraph(),
+            'image' => $this->faker->image(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
