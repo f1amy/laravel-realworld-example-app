e15e8ef7732927f3cfe8c4143c1e8cab9181a7f7
Flamy <27062841+f1amy@users.noreply.github.com>
2021-07-30 17:39:18 +0500

    finalize `DatabaseSeeder`
    - update composer dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Article;
 use App\Models\Comment;
 use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,10 +18,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Article::factory()
-            ->count(10)
-            ->has(Tag::factory()->count(3))
-            ->has(Comment::factory()->count(5))
+        /**
+         * @var User[]|\Illuminate\Database\Eloquent\Collection<User> $users
+         */
+        $users = User::factory()->count(20)->create();
+
+        foreach ($users as $user) {
+            $user->followers()->attach($users->random(rand(0, 5)));
+        }
+
+        /**
+         * @var Article[]|\Illuminate\Database\Eloquent\Collection<Article> $articles
+         */
+        $articles = Article::factory()
+            ->count(30)
+            ->state(new Sequence(function () use ($users) {
+                return ['author_id' => $users->random()];
+            }))
+            ->create();
+
+        /**
+         * @var Tag[]|\Illuminate\Database\Eloquent\Collection<Tag> $tags
+         */
+        $tags = Tag::factory()->count(20)->create();
+
+        foreach ($articles as $article) {
+            $article->tags()->attach($tags->random(rand(0, 6)));
+            $article->favoredUsers()->attach($users->random(rand(0, 8)));
+        }
+
+        Comment::factory()
+            ->count(60)
+            ->state(new Sequence(function () use ($articles, $users) {
+                return [
+                    'article_id' => $articles->random(),
+                    'author_id' => $users->random(),
+                ];
+            }))
             ->create();
     }
 }
