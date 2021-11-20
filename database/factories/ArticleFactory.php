e0532de92f5ba1646e300e8b93a4e7847ea045f7
Flamy <27062841+f1amy@users.noreply.github.com>
2021-11-20 15:33:21 +0500

    update base laravel installation - update composer dependencies - use automatic model name discovery for factories

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -10,13 +10,6 @@ use Illuminate\Support\Str;
 class ArticleFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Article::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
